dd37803a9aefd6b9e6860bcaa689fcb0eeb5628b
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2025-01-20 23:55:22 +0600

    add password hashing functionality (bcrypt)

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const bcrypt = require('bcrypt');
+
+
+require("dotenv").config();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.PASS}@cluster0.oeipnk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const myDB = client.db("FileVault");
+    const useColl = myDB.collection("registeredUser");
+
+    app.post("/users", async(req, res) => {
+      const userData = req.body;
+
+      const plainPass= userData.passCode;
+
+      const saltRounds = 10; 
+      const hashedPass = await bcrypt.hash(plainPass, saltRounds);
+
+      console.log(`Plain Password: ${plainPass}`);
+      console.log(`Hashed Password: ${hashedPass}`);
+
+      res.send({ data: { success: true, hashedPass } });
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
